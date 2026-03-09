180b822ab90a964bc3d20f86ed8519c5f4ab6897
bhb <selre1@naver.com>
2026-03-09 18:01:29 +0900

    # shaders 색상 조정

--- worker/jobs/ifc_import.py
+++ worker/jobs/ifc_import.py
@@ -155,6 +155,7 @@ class IfcImportJob:
         default_hex="#D3D3D3FF"
     ):
         basecolors = [default_hex] * tri_count
+        emissivecolors = [default_hex] * tri_count
         rgba_to_hex = self.rgba_to_hex
         material_cache_local = material_cache
         hex_cache = {}
@@ -171,10 +172,11 @@ class IfcImportJob:
 
             for idx in face_indices:
                 if 0 <= idx < tri_count:
+                    emissivecolors[idx] = "#1A1A1A"
                     basecolors[idx] = hexcolor
 
         return {
-            "EmissiveColors": basecolors,
+            "EmissiveColors": emissivecolors,
             "PbrMetallicRoughness": {
                 "BaseColors": basecolors
             }
